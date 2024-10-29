1112351af3d42b80b6f5ed4015f1b12ff928d3a1
czh <2244356600@qq.com>
2024-10-29 17:40:02 +0800

    新增欢迎页面

---
+++ src/views/welcome/index.tsx
@@ -0,0 +1,21 @@
+import React, { memo } from "react";
+import type { ReactNode, FC } from "react";
+import styles from "./index.module.less";
+interface Iprops {
+  children?: ReactNode;
+}
+const Welcome: FC<Iprops> = () => {
+  return (
+    <div className={styles.welcome}>
+      <div className="content">
+        <h1>欢迎使用后台管理系统</h1>
+        <p className={styles.desc}>
+          这是一个基于 React18 + ReactRouter6.0 + AntD5.2 + TypeScript + Vite
+          的后台管理系统模板。
+        </p>
+      </div>
+      <div className={styles.img} />
+    </div>
+  );
+};
+export default memo(Welcome);
